5e125693988b30d4996f20e449b271516463aeda
Zerc61 <sampbernad@gmail.com>
2025-11-19 17:23:28 +0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+app.get('/api/harga-emas', async (req, res) => {
+  try {
+    const url = 'https://freegoldprice.org/api/v2?key=O9ziscf1N9O12P94LdRUO5h02B93RSZTL5jcUYajvZem3VjNJUSZBLiNoBuk&action=GSPPJ';
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({ error: 'Gagal mengambil data emas' });
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
