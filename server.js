0daae815abb4aa18d378d9bf48e33420165b0f71
Zerc61 <sampbernad@gmail.com>
2025-11-23 08:18:33 +0700

    pembaruan key

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import axios from 'axios';
+import express from "express";
+import cors from "cors";
+import axios from "axios";
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 
-app.get('/api/harga-emas', async (req, res) => {
+app.get("/api/harga-emas", async (req, res) => {
   try {
     const url =
-      "https://freegoldprice.org/api/v2?key=zpYTTrghOVoNXj9UyY4ym3C0pf8qY7nrpDscotNi9KU5TsZoYRuWENYRCxPT&action=GSPPJ";
+      "https://freegoldprice.org/api/v2?key=80gWwX2J5QPkKIaAbVEE9K4ldAQXp1JQgsL5Tq3KcEdAp0IviEIb4hgu1rzk&action=GSPPJ";
     const response = await axios.get(url);
     res.json(response.data);
   } catch (err) {
-    res.status(500).json({ error: 'Gagal mengambil data emas' });
+    res.status(500).json({ error: "Gagal mengambil data emas" });
   }
 });
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
